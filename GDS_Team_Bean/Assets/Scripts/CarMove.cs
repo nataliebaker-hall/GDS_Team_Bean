f96a7d08f0160e17acd8ec0ef4f46b2f6408f262
unknown <1501144@MC8147.uad.ac.uk>
2017-02-25 21:29:26 +0000

    i dunno what this is but yolo amarite

---
+++ GDS_Team_Bean/Assets/Scripts/CarMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMove : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float input = Input.GetAxis ("Vertical");
+		transform.gameObject.GetComponent<Rigidbody2D> ().AddForce (gameObject.transform.up * speed * input);
+	}
+}
